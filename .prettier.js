ec659025fdb5140976f126dce2667b3ce4eed6a2
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-06-14 15:09:39 +0200

    Setup base platform for challenges
    Signed-off-by: Marcos Loureiro <mloureiro.dev@gmail.com>

---
+++ .prettier.js
@@ -0,0 +1,13 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	jsxBracketSameLine: false,
+	jsxSingleQuote: true,
+	printWidth: 60,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+}
---
+++ src/_setup.js
@@ -0,0 +1,24 @@
+// time : 00h00m00s
+
+const testFunction = () => {}
+
+const tests = [
+	{
+		input: [],
+		output: 0,
+	},
+]
+
+const isEqual = (a, b) =>
+	JSON.stringify(a) === JSON.stringify(b)
+
+tests.forEach(({ input, output }, idx) => {
+	let result = testFunction(...input)
+	const isEqualResult = isEqual(result, output)
+
+	console.log(
+		`Test ${idx + 1}:`,
+		isEqualResult ? '✓' : '❌',
+		!isEqualResult ? `-  ${output} ∙ ${result}` : '',
+	)
+})
